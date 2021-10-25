3d1697e3a5cc197456d6f890038b05923de71bfd
Alejandro Cerezo <alce65@hotmail.es>
2021-10-25 17:10:52 +0200

    Add previous basic component

---
+++ basic-component/index-jsx.js
@@ -0,0 +1,24 @@
+function Avatar(props) {
+  const src = `https://randomuser.me/api/portraits/women/${props.id}.jpg`;
+  const alt = `Avatar de ${props.name}`;
+
+  return (
+    <picture className="avatar" data-id={props.id}>
+      <h3>{props.name}</h3>
+      <img src={src} alt={alt} />
+    </picture>
+  );
+
+  /* return h("picture", { className: "avatar", "data-id": props.id }, [
+    h("h3", null, props.name),
+    h("img", { src: src, alt: alt }),
+  ]); */
+}
+const appx = document.querySelector("#avatars-jsx");
+ReactDOM.render(
+  <div>
+    <Avatar id={21} name="Luisa" />
+    <Avatar id="22" name="Maria" />
+  </div>,
+  appx
+);
---
+++ basic-component/index.js
@@ -0,0 +1,23 @@
+const h = React.createElement;
+
+function Avatar(props) {
+  const src = `https://randomuser.me/api/portraits/women/${props.id}.jpg`;
+  const alt = `Avatar de ${props.name}`;
+
+  /* <picture class="avatar" data-id="${id}"  >
+    <h3>${name}</h3>
+    <img src="${src}" alt=""
+  </picture> */
+  return h("picture", { className: "avatar", "data-id": props.id }, [
+    h("h3", null, props.name),
+    h("img", { src: src, alt: alt }),
+  ]);
+}
+const app = document.querySelector("#avatars");
+ReactDOM.render(
+  h("div", null, [
+    h(Avatar, { id: 11, name: "Pepa" }),
+    h(Avatar, { id: 12, name: "Elvira" }),
+  ]),
+  app
+);
